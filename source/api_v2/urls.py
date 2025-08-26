17332caef0b80b6e7264ec891a7df415d75d62bc
Arnat Asanov <asanovarnat2209@gmail.com>
2025-08-26 11:49:46 +0600

    Update API v2 urls for Article and Comment views

--- source/api_v2/urls.py
+++ source/api_v2/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path
 
-from api_v2.views import ArticleView, get_token_view
+from api_v2.views import (
+    get_token_view, ArticleView, CommentListCreateView, CommentDetailView)
 
 app_name = 'v2'
 
 urlpatterns = [
+    path('get-csrf/', get_token_view, name='get-csrf'),
+
     path('articles/', ArticleView.as_view(), name='articles'),
     path('articles/<int:pk>/', ArticleView.as_view(), name='article'),
-    path('get-csrf/', get_token_view, name='get-csrf'),
+
+    path('articles/<int:article_id>/comments/', CommentListCreateView.as_view(), name='article-comments'),
+    path('comments/<int:pk>/', CommentDetailView.as_view(), name='comment'),
 ]
