2e8d9ae91d8e14a834dc0c142e976ccf31c0bf05
Arnat Asanov <asanovarnat2209@gmail.com>
2025-08-26 11:49:52 +0600

    Fix and complete API v2 ArticleView and Comment views according to TЗ

--- source/api_v2/views.py
+++ source/api_v2/views.py
@@ -4,10 +4,9 @@ from rest_framework import status
 from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.views import APIView
-
-from api_v2.serializers import ArticleSerializer
-from webapp.models import Article
-
+from webapp.models import Article, Comment
+from api_v2.serializers.article import ArticleSerializer
+from api_v2.serializers.comment import CommentSerializer
 from django.views.decorators.csrf import ensure_csrf_cookie
 
 
@@ -15,27 +14,72 @@ from django.views.decorators.csrf import ensure_csrf_cookie
 def get_token_view(request, *args, **kwargs):
     if request.method == 'GET':
         return HttpResponse()
-    return HttpResponseNotAllowed('Only GET request are allowed')
+    return HttpResponseNotAllowed(['GET'])
 
 
 class ArticleView(APIView):
-    def get(self, request, *args, **kwargs):
+    def get(self, request, pk=None, *args, **kwargs):
+        if pk:
+            article = get_object_or_404(Article, pk=pk)
+            serializer = ArticleSerializer(article)
+            return Response(serializer.data, status=status.HTTP_200_OK)
         articles = Article.objects.all()
         serializer = ArticleSerializer(articles, many=True)
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     def post(self, request, *args, **kwargs):
         serializer = ArticleSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = get_user_model().objects.last()
         article = serializer.save(author=user)
-        return Response({"id": article.id}, status=status.HTTP_201_CREATED)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
     def put(self, request, pk, *args, **kwargs):
         article = get_object_or_404(Article, pk=pk)
-        serializer = ArticleSerializer(data=request.data, instance=article)
+        serializer = ArticleSerializer(instance=article, data=request.data, partial=True)
         if serializer.is_valid():
             article = serializer.save()
-            return Response(serializer.data)
-        else:
-            return Response({"errors": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response({"errors": serializer.errors}, status=status.HTTP_404_BAD_REQUEST)
+
+    def delete(self, request, pk, *args, **kwargs):
+        article = get_object_or_404(Article, pk=pk)
+        article_id = article.id
+        article.delete()
+        return Response({"id": article_id}, status=status.HTTP_204_NO_CONTENT)
+
+
+class CommentListCreateView(APIView):
+    def get(self, request, article_pk, *args, **kwargs):
+        comments = Comment.objects.filter(article_id=article_pk)
+        serializer = CommentSerializer(comments, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request, article_pk, *args, **kwargs):
+        article = get_object_or_404(Article, pk=article_pk)
+        serializer = CommentSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = get_user_model().objects.last()
+        comment = serializer.save(author=user, article=article)
+        return Response(CommentSerializer(comment).data, status=status.HTTP_201_CREATED)
+
+
+class CommentDetailView(APIView):
+    def get(self, request, pk, *args, **kwargs):
+        comment = get_object_or_404(Comment, pk=pk)
+        serializer = CommentSerializer(comment)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request, pk, *args, **kwargs):
+        comment = get_object_or_404(Comment, pk=pk)
+        serializer = CommentSerializer(instance=comment, data=request.data, partial=True)
+        if serializer.is_valid():
+            comment = serializer.save()
+            return Response(CommentSerializer(comment).data, status=status.HTTP_200_OK)
+        return Response({"errors": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, *args, **kwargs):
+        comment = get_object_or_404(Comment, pk=pk)
+        comment_id = comment.id
+        comment.delete()
+        return Response({"id": comment_id}, status=status.HTTP_204_NO_CONTENT)
